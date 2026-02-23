5bbff5f26d9abe8383dff17c24c2fc161e20e086
anilsimha13 <tanilsimha@gmail.com>
2026-02-23 16:05:34 +0530

    Day-00 | Warm-up

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ _Learning from NamasteDev Platform_
 
 ## Table of content
 
+- [1. Introduction](#1-introduction)
+- [2. Warm Up](#2-warm-up)
+
 ### 1. Introduction
 
 #### 1a. Introduction
@@ -23,3 +26,32 @@ _Learning from NamasteDev Platform_
 #### 2b. Programming 101
 
 - Location: [Programming101.js](/warm-up/programming101.js)
+- `let` can be changed
+- `const` cannot be changed
+- Any JS code will be executed only in JS engine
+
+##### Data types
+
+- String
+- Number
+- Boolean
+
+##### Arrays
+
+- Index starts from `Zero`
+
+- let arr = [2,12,14,15,55,67,69,98]
+
+##### Objects
+
+- Contains `key:value` pair
+
+```js
+let obj = {
+  firstName: "Anil",
+  lastName: "Simha",
+  boolean: true,
+  arr: [11, 12, 14],
+};
+//console.log(obj.firstName);
+```
--- warm-up/programming101.js
+++ warm-up/programming101.js
@@ -1,1 +1,71 @@
+//Console Statement
+
 console.log("Hello World");
+console.log("Anil Simha");
+console.log("Anil 1");
+console.log("Anil 2");
+console.log("July 31");
+
+//Arithmetic Operators
+
+const a = 10;
+const b = 20;
+
+const sum = a + b;
+const multiply = a * b;
+const difference = b - a;
+
+console.log("Sum of a and b is: ", sum);
+console.log("Multiplication of a and b is: ", multiply);
+console.log("difference of b and a is: ", difference);
+
+//let
+
+let c = 30;
+let d = 40;
+
+let result = d / c;
+console.log("divide d with c", result);
+
+let firstName = "Anil";
+let lastName = "Simha";
+
+console.log(firstName + " " + lastName);
+
+let DOB = 31;
+
+console.log("I was born on " + DOB + "st " + "JULY");
+
+//Arrays
+
+let arr = [2, 12, 14, 15, 55, 67, 69, 98];
+
+console.log(arr[3]);
+
+let sum1 = arr[2] + arr[5];
+
+console.log(sum1);
+
+//Arrays with Strings
+
+let arr1 = ["Anil", "Simha", "Tallapragada"];
+
+console.log(arr1[2]);
+
+//Arrays with different data types
+let arr2 = ["I am", 34, "Years", "Old", -1, true];
+
+//Arrays within Array
+let arr3 = ["Hello", ["Beach", "road"], "Vizag"];
+
+console.log(arr3[0], arr3[1][0]);
+
+//Objects
+let obj = {
+  firstName: "Anil",
+  lastName: "Simha",
+  boolean: true,
+  arr: [11, 12, 14],
+};
+
+console.log(obj.firstName);
